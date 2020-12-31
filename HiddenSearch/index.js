eb1555fd58f8404e50cea85126e4d7328a0200d3
DanishKhalique <61799810+DanishKhalique@users.noreply.github.com>
2020-12-31 19:23:10 +0530

    Hidden Search

---
+++ HiddenSearch/index.js
@@ -0,0 +1,7 @@
+const search = document.querySelector(".search")
+const button = document.getElementById("button")
+
+
+button.addEventListener('click', () => {
+    search.classList.toggle("active")
+})
